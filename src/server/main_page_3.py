e08a6dcc1ad665cba4e5707a0e208f66bfb529c2
XianZS <XianZhiSen_yang@outlook.com>
2025-02-25 19:40:38 +0800

    das

--- src/server/main_page_3.py
+++ src/server/main_page_3.py
@@ -11,17 +11,38 @@ import streamlit as st
 
 
 def __upload_things():
-    st.title("上传文件")
+    """
+        上传文件
+        st.file_uploader(
+            # 显示标题
+            label,
+            # 文件类型 参数为list
+            type=None,
+            # 设置是否允许多个文件上传
+            accept_multiple_files=False,
+        )
+        :return:
+    """
+    st.title("file_uploader")
+    st.subheader("上传图片")
     st.markdown("""
         ---
     """)
-    upload_image = st.file_uploader("上传图片", type=["png", "jpg", "jpeg"])
+    upload_image = st.file_uploader("上传图片",
+                                    type=["png", "jpg", "jpeg"],
+                                    accept_multiple_files=True)
     if upload_image:
-        st.write(upload_image.name)
-        st.write(upload_image.type)
-        st.write(upload_image.size)
-        # st.write(upload_image.getvalue())
-        st.image(upload_image)
+        # st.write(upload_image[0].name)
+        # st.write(upload_image[0].type)
+        # st.write(upload_image[0].size)
+        st.image(upload_image[0])
+
+    st.subheader("上传文件")
+    upload_file = st.file_uploader("上传文件",
+                                   type=["md", "txt", "py"],
+                                   accept_multiple_files=True)
+    if upload_file:
+        st.code(upload_file[0].getvalue().decode("utf8"), language="python")
 
 
 def main_page_3_show():
