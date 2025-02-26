8279b5665609994ab4f9b950de3e772611bad438
XianZS <XianZhiSen_yang@outlook.com>
2025-02-26 22:50:14 +0800

    2025-2-26

--- src/server/main_page_3.py
+++ src/server/main_page_3.py
@@ -5,8 +5,12 @@
     @IDE     : PyCharm 
     @Author  : XianZS
     @Date    : 2025/2/22 21:01 
-    @NowThing: 
+    @NowThing: 1.st.file_uploader 上传文件方法
+               2.st.slider 滑动条方法
+               3.
 """
+from typing import Union
+
 import streamlit as st
 
 
@@ -18,12 +22,11 @@ def __upload_things():
             label,
             # 文件类型 参数为list
             type=None,
-            # 设置是否允许多个文件上传
+            # 设置是否允许多个文件上传,默认为False不允许多文件上传
             accept_multiple_files=False,
         )
         :return:
     """
-    st.title("file_uploader")
     st.subheader("上传图片")
     st.markdown("""
         ---
@@ -37,7 +40,6 @@ def __upload_things():
         # st.write(upload_image[0].size)
         st.image(upload_image[0])
 
-    st.subheader("上传文件")
     upload_file = st.file_uploader("上传文件",
                                    type=["md", "txt", "py"],
                                    accept_multiple_files=True)
@@ -45,8 +47,84 @@ def __upload_things():
         st.code(upload_file[0].getvalue().decode("utf8"), language="python")
 
 
+def __slider_things() -> None:
+    """
+        滑动条方法
+        st.slider(
+            # 显示标题
+            label,
+            # 最小值
+            min_value=None,
+            # 最大值
+            max_value=None,
+            # 步长
+            step=None,
+            # 滑动条的默认值
+            value=None,
+            # 滑动条的格式
+            format=None,
+            # 滑动条的key
+            key=None,
+            # 滑动条的帮助信息
+            help=None,
+            # 滑动条的回调函数
+            on_change=None,
+            # 滑动条的参数
+            args=None,
+            # 滑动条的kwargs
+            kwargs=None,
+            # 滑动条的禁用状态
+            disabled=False,
+            # 滑动条的标签
+            label_visibility="visible",
+        )
+        :return:
+    """
+    st.subheader("滚动条")
+    st.markdown("""
+        ---
+    """)
+    val = st.slider("This is a slider.",
+                    # 最小可选值
+                    min_value=20,
+                    # 最大可选值
+                    max_value=100,
+                    # 初始值
+                    value=70)
+    st.write(f"当前选择值为 : {val}")
+
+
+def __input_things() -> None:
+    """
+        st.text_input("文本输入",max_chars=最大输入上限)
+        :return:
+    """
+    st.subheader("输入框")
+    st.markdown("""
+        ---
+    """)
+    val = st.text_input("输入")
+    st.write(f"你的输入为:{val}")
+
+    def clear_input() -> None:
+        if st.session_state.user_input_things:
+            st.session_state.input_things = st.session_state.user_input_things
+        st.session_state.user_input_things = ""
+
+    st.text_input("用户",
+                  key="user_input_things",
+                  max_chars=1000,
+                  on_change=clear_input)
+    if "input_things" in st.session_state:
+        st.write(f"st.session_state type is {type(st.session_state)}")
+        st.write(f"st.session_state things is {st.session_state}")
+        st.write(f"你的输入 : {st.session_state.input_things}")
+
+
 def main_page_3_show():
     __upload_things()
+    __slider_things()
+    __input_things()
 
 
 if __name__ == "__main__":
